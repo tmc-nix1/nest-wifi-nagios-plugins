9e1206b36afe2e1571be06e44649026f0e54898f
tmc-nix1 <70933965+tmc-nix1@users.noreply.github.com>
2020-09-09 09:14:31 +1000

    Add files via upload

---
+++ check_nestpoint.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import time
+import pprint
+import os.path
+import datetime
+import sys
+
+# Construct the URL
+ip1 = sys.argv[1]
+#ip1 = "192.168.86.29"
+
+url1 = "http://%s/api/v1/status" % (ip1)
+
+#print(url1)
+
+## Get the JSON response from Nest Router
+nestresponse = requests.get(url1).json()
+
+## Uptime string analysis
+
+# This is Not human readable so will convert it
+#print("System Uptime is:")
+#print(nestresponse["system"]["uptime"])
+
+uptimenest = nestresponse["system"]["uptime"]
+uptimehuman = str(datetime.timedelta(seconds=uptimenest))
+
+#print("Nest Uptime is:")
+print(uptimehuman)
+
